79a8c58d718e890f37c6ee0b2d6afcc7e2064029
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2022-07-01 15:41:26 -0500

    Added explosion image resource file and prepped sandbox for sprite animation tutorial

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -7,18 +7,31 @@ import com.soywiz.korim.color.*
 import com.soywiz.korim.format.*
 import com.soywiz.korio.file.std.*
 import com.soywiz.korma.geom.*
-import com.soywiz.korma.geom.shape.Shape2d
 import com.soywiz.korma.interpolation.*
 
 suspend fun main() = Korge(width = 512, height = 512, bgcolor = Colors["#2b2b2b"]) {
 
+    // Basic Shapes and Images:
 //	val circle = Circle(radius = 20.0, fill = Colors.GREEN).xy(100, 100)
 //	addChild(circle)
 	// ^^ code above does the same as:
+//	circle(radius = 20.0, fill = Colors.GREEN).xy(100, 100)  // can use this syntax with shapes and views that can be added to screen
+//
+//
+//    solidRect(width = 100.0, height = 100.0, Colors.GOLD).xy(110, 110)  // the later a view is added to it's parent, the higher it is in the drawing stack
+//
+//    val bitmap = resourcesVfs["korge.png"].readBitmap()
+//    image(bitmap).scale(.3).apply {
+//       // rotation = (+50).degrees
+//        alpha = 0.5
+//    }
 
-	circle(radius = 20.0, fill = Colors.GREEN).xy(100, 100)  // can use this syntax with shapes and views that can be added to screen
+// Sprites and SpriteAnimations:
 
-    solidRect(width = 100.0, height = 100.0, Colors.GOLD).xy(110, 100)
+
+
+
+
 }
 
 
